3e1d8401e9b132114a5aa13f6857c6a16232305b
nicovrg <nicolasvergne88@gmail.com>
2019-11-06 02:47:30 +0100

    fucking makefile

--- src/asm/main.c
+++ src/asm/main.c
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qgirard <qgirard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nicolasv <nicolasv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 14:21:47 by qgirard           #+#    #+#             */
-/*   Updated: 2019/11/05 14:52:11 by qgirard          ###   ########.fr       */
+/*   Updated: 2019/11/06 02:47:21 by nicolasv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 #include "libft.h"
+
+/*
+** ==================== Oooookina kisu ====================
+** yo j'ai check vite fait sur le makefile à la maison, pas réussi à me concentrer dessus à l'école
+** (la faute à timothée, guillaume, kevin, allan, maxime, steven)
+** ça me pète trop les couilles c'est la même structure de dossier que lemin
+** c'est quasi le même Makefile donc ça doit être un truc débile qu'on verra pas ...
+** en attendant tu peux toujours compiler à l'ancienne depuis le dossier src/asm
+** gcc src/asm/main.c src/asm/error.c -Iinclude -I libft/include -L libft/ -lft -o yipikai
+*/
 
 int		main(int argc, char **argv)
 {
