9390e61af82a4164eeba6c8443e998efa9167340
Quentin GIRARD <qgirard@e2r12p22.42.fr>
2020-01-22 00:43:30 +0100

    rip allan

---
+++ src/vm/read_champions.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_champions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qgirard <qgirard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 16:19:15 by qgirard           #+#    #+#             */
+/*   Updated: 2019/11/13 14:52:28 by qgirard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+#include "libft.h"
+
+int		read_champions(char *argv, t_corewar *stock)
+{
+	int			fd;
+	char		*line;
+
+	fd = 0;
+	if (argv && (fd = open(argv, O_RDONLY)) == -1)
+		return (error_msg(ERR_OPEN_FILE, 0));
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+	close(fd);
+	stock->n_option = 0;
+	stock->nb_player = 0;
+	return (1);
+}
