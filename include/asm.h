ddba75ecd415c26d1d923491c61f760483f7b221
Nicolas VERGNE <nicolasvergne88@gmail.com>
2020-01-22 06:31:40 +0100

    start .h

--- include/asm.h
+++ include/asm.h
@@ -6,7 +6,7 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 01:21:58 by nivergne          #+#    #+#             */
-/*   Updated: 2020/01/21 02:55:41 by nivergne         ###   ########.fr       */
+/*   Updated: 2020/01/22 06:30:44 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,27 @@
 # define ERR_FILE_TYPE "not a valid file, the file should be a .s"
 # define ERR_OPEN_FILE "file doesn't exist"
 
+typedef struct			s_token
+{
+	int					value;
+	char				*lexme;
+	struct s_token		*next;
+}						t_token;
+
+typedef struct			s_line
+{
+	int					nb_line;
+	char				*line;
+	char				*label;
+	struct s_token		*token;
+	struct s_line		*next;
+}						t_line;
+
+
 /* asm_main.c */
 
-int				error_msg(char *error_msg, int i);
-int				vm_usage(int i);
+
+int						error_msg(char *error_msg, int i);
+int						vm_usage(int i);
 
 #endif
--- src/asm/asm_main.c
+++ src/asm/asm_main.c
@@ -6,26 +6,46 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 14:21:47 by qgirard           #+#    #+#             */
-/*   Updated: 2020/01/22 05:41:52 by nivergne         ###   ########.fr       */
+/*   Updated: 2020/01/22 06:28:53 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include "libft.h"
 
+int	parser(void) {return (0);};
+int	translator(void) {return (0);}
+
+int	lexer(t_line *codeline) 
+{
+	while ()
+	{
+		
+	}
+	return (1);
+}
+
 int		main(int argc, char **argv)
 {
 	int		fd;
+	t_line	codeline;
 
 	if (argc != 2)
 		return (error_msg(ERR_NB_PARAMETERS, 1));
 	else
 	{
-		if (argv[1] && ft_strchr(argv[1], '.')
-		&& ft_strcmp(ft_strchr(argv[1], '.'), ".s"))
+		if (argv[1] && ft_strchr(argv[1], '.') && ft_strcmp(ft_strchr(argv[1], '.'), ".s"))
 			return (error_msg(ERR_FILE_TYPE, 1));
 		else if (argv[1] && (fd = open(argv[1], O_RDONLY)) == -1)
 			return (error_msg(ERR_OPEN_FILE, 1));
+		ft_putstr("coucou\n");
+		// init
+		if (!lexer(&codeline))
+			return 0;
+		if (!parser())
+			return 0;
+		if (!translator())
+			return 0;
 	}
 	return (0);
 }
