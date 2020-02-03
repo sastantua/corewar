b5ba317e3d49fae3e55ddf9037f25cac1ac6c6a1
nicovrg <nicolasvergne88@gmail.com>
2020-02-03 03:48:03 +0100

    rename some files for clarity

--- mk/src.mk
+++ mk/src.mk
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    src.mk                                             :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+         #
+#    By: nicolasv <nicolasv@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/22 23:46:29 by nivergne          #+#    #+#              #
-#    Updated: 2020/02/02 05:16:40 by nivergne         ###   ########.fr        #
+#    Updated: 2020/02/03 03:47:48 by nicolasv         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,17 +14,17 @@ SRC_PATH		:=	src/
 SRC_ASM_PATH	:=	asm/
 SRC_VM_PATH		:=	vm/
 
-SRC_ASM =			asm_main.c\
-					asm_header_one.c\
-					asm_header_two.c\
-					asm_lexer.c\
-					asm_tokeniser.c\
-					asm_determine_token_type.c\
-					asm_tokeniser_states-function_one.c\
-					asm_tokeniser_states-function_two.c\
-					asm_lib.c\
-					asm_debug.c\
-					asm_error.c
+SRC_ASM =			main.c\
+					mini_lib.c\
+					debug.c\
+					error.c\
+					lexer.c\
+					get_header_one.c\
+					get_header_two.c\
+					get_tokens_from_current_line.c\
+					determine_token_type_one.c\
+					determine_token_type_func_two.c\
+					determine_token_type_func_three.c\
 
 SRC_VM =			vm_main.c\
 					vm_checks.c\
