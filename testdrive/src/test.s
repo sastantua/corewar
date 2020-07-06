78f05fdd5d82330d23fbbef13482ed9b107b8232
Allan <gitlab@amamy.fr>
2020-07-06 15:31:58 +0200

    In last chercker commit, forgot to save checker.py so we miss the "?" in FAIL

--- testdrive/src/test.s
+++ testdrive/src/test.s
@@ -1,4 +1,11 @@
 .name "maxidef"
 .comment "module de defense"
 
-entree: st r1,6
+ld :buff,r1
+goboucle0:	live %66
+		zjmp %:finboucle0
+		zjmp %:goboucle0
+finboucle0:	xor r3,r3,r3
+
+buff:	st r1,r1
+
